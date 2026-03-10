ee9dffe1ead8e607af74e70a47cc70e9192f132a
ArMaTeC <armatec0@gmail.com>
2026-03-10 21:57:43 +0000

    Bump version to 2.0.41

--- Redball.ps1
+++ Redball.ps1
@@ -76,7 +76,7 @@ param(
     [switch]$ExitOnComplete
 )
 
-$script:VERSION = '2.0.40'
+$script:VERSION = '2.0.41'
 $script:APP_NAME = 'Redball'
 $script:IsPS7 = $PSVersionTable.PSVersion.Major -ge 7
 
