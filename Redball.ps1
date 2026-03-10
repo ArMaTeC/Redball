d3bf18c300a0e808bcef2ccdaada1034f0b11fb1
Karl Lawrence <armatec0@gmail.com>
2026-03-10 11:26:38 +0000

    Bump version to 2.0.5 to test WiX fixes

--- Redball.ps1
+++ Redball.ps1
@@ -30,7 +30,7 @@
     .\Redball.ps1 -Status | ConvertFrom-Json
     Get current Redball status programmatically.
 .NOTES
-    Version: 2.0.4
+    Version: 2.0.5
     Requires: PowerShell 5.1+, Windows 8.1+
 #>
 [CmdletBinding()]
@@ -76,7 +76,7 @@ param(
     [switch]$ExitOnComplete
 )
 
-$script:VERSION = '2.0.4'
+$script:VERSION = '2.0.5'
 $script:APP_NAME = 'Redball'
 $script:IsPS7 = $PSVersionTable.PSVersion.Major -ge 7
 
