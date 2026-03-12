3e7641f368545d827aa28a48087504dd13dbc854
ArMaTeC <armatec0@gmail.com>
2026-03-12 08:08:05 +0000

    Remove obj folder from tracking

--- src/Redball.UI.WPF/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-// Resolve ambiguous type references between WPF and WinForms namespaces
-global using Application = System.Windows.Application;
-global using Color = System.Windows.Media.Color;
-global using MessageBox = System.Windows.MessageBox;
-global using TextBox = System.Windows.Controls.TextBox;
